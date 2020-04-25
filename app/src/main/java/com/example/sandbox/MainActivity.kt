d3ac50734bbb7cea18aefa436b2e72ed98e2becf
Vitaly <vitalmago.job@gmail.com>
2020-04-25 19:50:19 +0600

    Initial commit

---
+++ app/src/main/java/com/example/sandbox/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sandbox
+
+import android.content.res.AssetManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
